bafef6fa34383509820b71c34318976b0549480b
Alberto-R-L <albertorl003@gmail.com>
2020-11-08 15:21:15 +0100

    a

---
+++ Ejercicio 2.js
@@ -0,0 +1,20 @@
+
+//Worksheet 1 tema 4
+//Ejercicio 1
+
+/*Pegar en html
+
+
+        <div onmousemove="funcionMov(event)">JUAN <br> <br> <br> <br> <br> JUAN</div>
+        
+
+
+*/
+
+
+
+function funcionMov() {
+    var x = event.clientX;
+    var y = event.clientY;
+    console.log("Pos x: " + x + " Pos y: " + y);
+}
